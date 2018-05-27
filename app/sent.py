c867cd31701d23a96f5f01288f1deb02bcff4420
FacistFuhrer <mikebao1999@gmail.com>
2018-05-27 00:43:48 -0700

    new feature

--- app/sent.py
+++ app/sent.py
@@ -1,5 +1,17 @@
 import json
 from watson_developer_cloud import ToneAnalyzerV3
+import textrazor
+
+def analyzeText(text):
+  textrazor.api_key = "b695217cdaeb234d8a4edd867e1ab59b23aa1d050fa063c5f4a3a89a"
+  client = textrazor.TextRazor(extractors=["topics"])
+  response = client.analyze(text)
+  topic = map(lambda x: str(x.label), response.topics()[:5])
+  ans = ""
+  for topica in topic:
+    ans += topica + "\n"
+  return ans
+
 
 colors = {'Anger':'#FFE0E6', 'Confident':'#FEE7C1', 'Analytical':'#EBE1FE', 'Fear':'#DCF2F2', 'Tentative':'#FFEEFE', 'Sadness':'#D8ECFA', 'Joy':'#FFF5DF'}
 
--- app/views.py
+++ app/views.py
@@ -18,9 +18,9 @@ def file_allowed(file):
 @views.route('/', methods=["GET", "POST"])
 def homepage():
 	if request.method == "POST":
-		return render_template('homepage.html', WordCount = sent.colorText(request.form.get("emailtext")), piechart = str(sent.sentList(request.form.get("emailtext"))))
+		return render_template('homepage.html', WordCount = sent.colorText(request.form.get("emailtext")), piechart = str(sent.sentList(request.form.get("emailtext"))), topics = sent.analyzeText(request.form.get("emailtext")))
 	else:
-		return render_template('homepage.html', WordCount = "", piechart = "[0,0,0,0,0,0,0]")
+		return render_template('homepage.html', WordCount = "", piechart = "[0,0,0,0,0,0,0]", topics = "")
 
 @views.route('/about')
 def about():
